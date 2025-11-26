379499161d6ba569cf3b0adc7055a564b8aea4f3
Osnigilberto <uuujuninho@gmail.com>
2025-11-26 03:06:24 -0300

    edit analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
   title: 'Osni Gilberto Ferreira Junior - Frontend Developer',
@@ -49,6 +50,7 @@ export default function RootLayout({ children }) {
       </head>
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   )
